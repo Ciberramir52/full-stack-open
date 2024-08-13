84bd8d6c2df6236cc2cbe5287c3d54e9f3560cc1
Ciberramir52 <63085235+Ciberramir52@users.noreply.github.com>
2024-08-13 13:41:24 -0600

    Connect to MongoDB

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const Person = require('./models/persons');
 const app = express();
+const cors = require('cors');
 const morgan = require('morgan');
 
-let persons = [
+let localPersons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -25,9 +28,9 @@ let persons = [
     }
 ];
 
-morgan.token('body', req => {
-    return JSON.stringify(req.body)
-});
+app.use(cors())
+
+morgan.token('body', req => JSON.stringify(req.body));
 
 function morganForPostRequests(req, res, next) {
     if (req.method === 'POST') {
@@ -35,7 +38,6 @@ function morganForPostRequests(req, res, next) {
     } else {
         morgan('tiny')(req, res, next);
     }
-    next();
 }
 
 app.use(express.static('dist'));
@@ -49,12 +51,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    Person.find({}).then(result => {
+        res.json(result);        
+    })
 });
 
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
+    const person = localPersons.find(person => person.id === id);
     console.log(person);
     if (!person) return res.sendStatus(404);
     return res.json(person);
@@ -62,7 +66,7 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
+    localPersons = localPersons.filter(person => person.id !== id);
     res.sendStatus(204);
 });
 
@@ -71,24 +75,24 @@ app.post('/api/persons', (req, res) => {
     if (!person || person.name === '' || person.number === '') return res.status(400).json({
         error: 'content missing'
     });
-    const checkPerson = persons.find(element => element.name === person.name);
+    const checkPerson = localPersons.find(element => element.name === person.name);
     if (checkPerson) return res.status(400).json({
         error: 'name must be unique'
     });
     const id = Math.floor(Math.random() * 5000);
     const result = { id, ...person };
-    persons = persons.concat(result)
+    localPersons = localPersons.concat(result)
     res.json(result);
 })
 
 app.get('/info', (req, res) => {
     res.send(`
-        <p>Phonebook has info for ${persons.length}</p>
+        <p>Phonebook has info for ${localPersons.length}</p>
         <p>${new Date()}</p>
     `);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
