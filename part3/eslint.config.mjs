093f9294526d4edeba8e44425d2819189df2b999
Ciberramir52 <63085235+Ciberramir52@users.noreply.github.com>
2024-08-14 14:00:12 -0600

    uibuild

---
+++ part3/eslint.config.mjs
@@ -0,0 +1,63 @@
+import stylistic from "@stylistic/eslint-plugin-js";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    plugins: {
+        "@stylistic/js": stylistic,
+    },
+
+    ignores: [".eslintrc.js", "eslint.config.mjs", "dist", "models"],
+
+    languageOptions: {
+        globals: {
+            ...globals.commonjs,
+            ...globals.node,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "commonjs",
+        parserOptions: {
+            "sourceType": "module",
+        }
+    },
+
+    rules: {
+        "@stylistic/js/indent": ["error", 2],
+        "@stylistic/js/linebreak-style": ["error", "unix"],
+        "@stylistic/js/quotes": ["error", "single"],
+        "@stylistic/js/semi": ["error", "never"],
+        eqeqeq: "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": ["error", "always"],
+
+        "arrow-spacing": ["error", {
+            before: true,
+            after: true,
+        }],
+
+        "no-console": 0,
+    },
+}, {
+    files: ["**/.eslintrc.{js,cjs}"],
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        ecmaVersion: 5,
+        sourceType: "commonjs",
+    },
+}];
--- part3/index.js
+++ part3/index.js
@@ -1,141 +1,137 @@
-require('dotenv').config();
-const express = require('express');
-const Person = require('./models/persons');
-const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/persons')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 
-app.use(cors());
+app.use(cors())
 
-morgan.token('body', req => JSON.stringify(req.body));
+morgan.token('body', req => JSON.stringify(req.body))
 
 function morganForPostRequests(req, res, next) {
-    if (req.method === 'POST') {
-        morgan(':method :url :status :res[content-length] - :response-time ms :body')(req, res, next); // Aquí puedes usar cualquier formato de Morgan, como 'combined'
-    } else {
-        morgan('tiny')(req, res, next);
-    }
+  if (req.method === 'POST') {
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')(req, res, next) // Aquí puedes usar cualquier formato de Morgan, como 'combined'
+  } else {
+    morgan('tiny')(req, res, next)
+  }
 }
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
-app.use(express.json());
+app.use(express.json())
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(morganForPostRequests);
+app.use(morganForPostRequests)
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
+  res.send('<h1>Hello World!</h1>')
+})
 
 app.get('/info', (req, res, next) => {
-    Person.find({}).then(result => {
-        res.send(`
+  Person.find({}).then(result => {
+    res.send(`
             <p>Phonebook has info for ${result.length} persons</p>
             <p>${new Date()}</p>
-        `);
-    }).catch(error => next(error));
-});
+        `)
+  }).catch(error => next(error))
+})
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({}).then(result => {
-        res.json(result);
-    }).catch(error => next(error))
-});
+  Person.find({}).then(result => {
+    res.json(result)
+  }).catch(error => next(error))
+})
 
 app.get('/api/persons/:id', (req, res, next) => {
-    // const id = Number(req.params.id);
-    // const person = localPersons.find(person => person.id === id);
-    // console.log(person);
-    // if (!person) return res.sendStatus(404);
-    // return res.json(person);
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(error => next(error))
+  // const id = Number(req.params.id);
+  // const person = localPersons.find(person => person.id === id);
+  // console.log(person);
+  // if (!person) return res.sendStatus(404);
+  // return res.json(person);
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    // const id = Number(req.params.id);
-    // localPersons = localPersons.filter(person => person.id !== id);
-    // res.sendStatus(204);
-    Person.findByIdAndDelete(req.params.id)
-        .then(result => {
-            if (!result) return res.status(404).json({ error: 'Already removed from server' });
-            res.status(204).end();
-        })
-        .catch(error => next(error));
-});
+  // const id = Number(req.params.id);
+  // localPersons = localPersons.filter(person => person.id !== id);
+  // res.sendStatus(204);
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      if (!result) return res.status(404).json({ error: 'Already removed from server' })
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body;
-    console.log(body.name, body.number);
+  const body = req.body
 
-    if (!body.name || !body.number || body.name === '' || body.number === '') {
-        console.log(body.name, body.number);
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
-    }
-    Person.find({ name: body.name })
-        .then(result => {
-            console.log(result);
-
-            if (result.length !== 0) return res.status(400).json({
-                error: 'name must be unique'
-            });
-            const uploadPerson = new Person(body);
-            uploadPerson.save().then(resultUpload => {
-                // console.log('Person saved!', result)
-                // mongoose.connection.close()
-                res.json(resultUpload);
-            }).catch(error => next(error));
-        }).catch(error => next(error));
+  if (!body.name || !body.number || body.name === '' || body.number === '') {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  Person.find({ name: body.name })
+    .then(result => {
+      if (result.length !== 0) return res.status(400).json({
+        error: 'name must be unique'
+      })
+      const uploadPerson = new Person(body)
+      uploadPerson.save().then(resultUpload => {
+        // console.log('Person saved!', result)
+        // mongoose.connection.close()
+        res.json(resultUpload)
+      }).catch(error => next(error))
+    }).catch(error => next(error))
     // const id = Math.floor(Math.random() * 5000);
     // const result = { id, ...person };
     // localPersons = localPersons.concat(result)
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = { ...body }
+  const person = { ...body }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 // controlador de solicitudes con endpoint desconocido
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message });
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error);
+  next(error)
 }
 
 // este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
 app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-});
+  console.log(`Server running on port ${PORT}`)
+})
